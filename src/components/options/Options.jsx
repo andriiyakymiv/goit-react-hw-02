c48d9b13094aa245850285660dac8dfc5012ca48
andrii <a.iakumiv@gmail.com>
2025-04-21 12:26:06 +0300

    Changed the styling for the feedback form

--- src/components/options/Options.jsx
+++ src/components/options/Options.jsx
@@ -1,16 +1,15 @@
 import styles from "./Options.module.css";
 
 const Options = ({ updateFeedback, totalFeedback, resetNotes }) => {
+
   const options = ["good", "neutral", "bad"];
+
   return (
-    <ul className={styles.optionsList}>
-      {options.map((option) => {
+
+    <ul className={styles.options__list}> {options.map((option) => {
         return (
           <li key={option}>
-            <button
-              className={styles.optionBTN}
-              onClick={() => updateFeedback(option)}
-            >
+            <button className={styles.option__button} onClick={() => updateFeedback(option)}>
               {option}
             </button>
           </li>
@@ -19,7 +18,7 @@ const Options = ({ updateFeedback, totalFeedback, resetNotes }) => {
 
       {totalFeedback !== 0 && (
         <li key="reset">
-          <button className={styles.optionBTN} onClick={resetNotes}>
+          <button className={styles.button__reset} onClick={resetNotes}>
             Reset points
           </button>
         </li>
